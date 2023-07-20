855d04d6637b978abb5ac6eee02b0ecde842a002
Harran Ali <harran.m@gmail.com>
2023-07-20 15:23:47 +0400

    implement message building

--- message.go
+++ message.go
@@ -1,14 +1,24 @@
 package mailing
 
-import "net/mail"
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/mail"
+	"os"
+	"strings"
+)
 
 type messageBuilder struct {
 	subject       string
 	htmlBody      string
 	plainTextBody string
-	from          mail.Address
-	toList        []mail.Address
-	ccList        []mail.Address
+	from          string
+	toList        []string
+	ccList        []string
 	attachments   []Attachment
 }
 
@@ -36,17 +46,25 @@ func (m *messageBuilder) setPlainTextBody(body string) *messageBuilder {
 }
 
 func (m *messageBuilder) setFrom(from mail.Address) *messageBuilder {
-	m.from = from
+	m.from = prepareAddressString(from)
 	return m
 }
 
 func (m *messageBuilder) setToList(toList []mail.Address) *messageBuilder {
-	m.toList = toList
+	var toListStr []string
+	for _, v := range toList {
+		toListStr = append(toListStr, prepareAddressString(v))
+	}
+	m.toList = toListStr
 	return m
 }
 
 func (m *messageBuilder) setCCList(ccList []mail.Address) *messageBuilder {
-	m.ccList = ccList
+	var ccListStr []string
+	for _, v := range ccList {
+		ccListStr = append(ccListStr, prepareAddressString(v))
+	}
+	m.ccList = ccListStr
 	return m
 }
 
@@ -55,7 +73,48 @@ func (m *messageBuilder) setAttachments(attachments []Attachment) *messageBuilde
 	return m
 }
 
-func (m *messageBuilder) build() string {
-	// TODO implement
-	return ""
+func (m *messageBuilder) build() []byte {
+	buf := bytes.NewBuffer(nil)
+	buf.WriteString(fmt.Sprintf("From: %s\r\n", m.from))
+	buf.WriteString(fmt.Sprintf("To: %s\r\n", strings.Join(m.toList, ";")))
+	if len(m.ccList) > 0 {
+		buf.WriteString(fmt.Sprintf("CC: %s\r\n", strings.Join(m.ccList, ";")))
+	}
+	buf.WriteString(fmt.Sprintf("Subject: %s\r\n", m.subject))
+	writer := multipart.NewWriter(buf)
+	boundary := writer.Boundary()
+	buf.WriteString("MIME-Version: 1.0\r\n")
+	buf.WriteString(fmt.Sprintf("Content-Type: multipart/mixed; boundary=%s", boundary))
+	if m.htmlBody != "" {
+		buf.WriteString(fmt.Sprintf("\r\n--%s", boundary))
+		buf.WriteString("Content-Type: text/html; charset=UTF-8")
+		buf.WriteString(m.htmlBody)
+	} else {
+		buf.WriteString(fmt.Sprintf("\r\n--%s", boundary))
+		buf.WriteString("Content-Type: text/plain; charset=UTF-8")
+		buf.WriteString(m.plainTextBody)
+	}
+	if len(m.attachments) > 0 {
+		for _, attachment := range m.attachments {
+			file, err := os.Open(attachment.Path)
+			fileContent, err := io.ReadAll(file)
+			if err != nil {
+				panic(err.Error())
+			}
+			if err != nil {
+				panic(err.Error())
+			}
+			buf.WriteString(fmt.Sprintf("\r\n--%s", boundary))
+			buf.WriteString(fmt.Sprintf("Content-Type: %s", http.DetectContentType(fileContent)))
+			buf.WriteString("Content-Transfer-Encoding: base64\r\n")
+			buf.WriteString(fmt.Sprintf("Content-Disposition: attachment; filename=%s", attachment.Name))
+
+			b := make([]byte, base64.StdEncoding.EncodedLen(len(fileContent)))
+			base64.StdEncoding.Encode(b, fileContent)
+			buf.Write(b)
+			file.Close()
+		}
+	}
+	buf.WriteString(fmt.Sprintf("\r\n--%s--", boundary))
+	return buf.Bytes()
 }
