5c4dc3386dbfe909e05a9d8b5646df19c5c101cb
Harran Ali <harran.m@gmail.com>
2023-07-21 23:43:17 +0400

    test smtp driver

--- smtp-driver.go
+++ smtp-driver.go
@@ -31,15 +31,57 @@ type smtpDriver struct {
 	htmlBody       string
 	plainTextBody  string
 	attachments    []Attachment
+	initiateSend   func(from string, rcpts []string, message []byte, conf SMTPConfig) error
+}
+
+var initiateSend = func(from string, rcpts []string, message []byte, conf SMTPConfig) error {
+	conn, err := tls.Dial("tcp", fmt.Sprintf("%s:%d", conf.Host, conf.Port), &conf.TLSConfig)
+	if err != nil {
+		return errors.New(fmt.Sprintf("error calling tls.Dial(): %v", err.Error()))
+	}
+	defer conn.Close()
+	client, err := smtp.NewClient(conn, conf.Host)
+	if err != nil {
+		return errors.New(fmt.Sprintf("error calling smtp.NewClient(): %v", err.Error()))
+	}
+	defer client.Close()
+	err = client.Auth(smtp.PlainAuth("", conf.Username, conf.Password, conf.Host))
+	if err != nil {
+		return errors.New(fmt.Sprintf("error calling SMTP's client.Auth(): %v", err.Error()))
+	}
+	client.Mail(from)
+	for _, emailAddress := range rcpts {
+		err = client.Rcpt(emailAddress)
+		if err != nil {
+			return errors.New(fmt.Sprintf("error calling rcpt(): %v", err.Error()))
+		}
+	}
+	writer, err := client.Data()
+	if err != nil {
+		return errors.New(fmt.Sprintf("error calling data(): %v", err.Error()))
+	}
+	_, err = writer.Write(message)
+	if err != nil {
+		return errors.New(fmt.Sprintf("error calling writer.Close(): %v", err.Error()))
+	}
+	writer.Close()
+	err = client.Quit()
+	if err != nil {
+		return errors.New(fmt.Sprintf("error quiting client: %v", err.Error()))
+	}
+	return nil
 }
 
 func initiateSMTP(config *SMTPConfig) *smtpDriver {
-	return &smtpDriver{
+	s := &smtpDriver{
 		config:         config,
 		messageBuilder: newMessageBuilder(),
 		htmlBody:       "",
 		plainTextBody:  "",
+		initiateSend:   initiateSend,
 	}
+
+	return s
 }
 
 func (s *smtpDriver) SetFrom(from mail.Address) *smtpDriver {
@@ -99,55 +141,18 @@ func (s *smtpDriver) Send() error {
 	for _, v := range s.ccList {
 		rcpts = append(rcpts, v.String())
 	}
-	err := s.initiateSend(rcpts, message)
+	from := s.from.String()
+	err := s.initiateSend(from, rcpts, message, *s.config)
 	if err != nil {
 		return errors.New(fmt.Sprintf("error calling s.initiateSend(): %v", err.Error()))
 	}
 
 	// send to bcc
 	for _, v := range s.bccList {
-		err = s.initiateSend([]string{v.String()}, message)
+		err = s.initiateSend(from, []string{v.String()}, message, *s.config)
 		if err != nil {
 			return errors.New(fmt.Sprintf("error calling s.initiateSend(): %v", err.Error()))
 		}
-	}
-	return nil
-}
-
-func (s *smtpDriver) initiateSend(rcpts []string, message []byte) error {
-	conn, err := tls.Dial("tcp", fmt.Sprintf("%s:%d", s.config.Host, s.config.Port), &s.config.TLSConfig)
-	if err != nil {
-		return errors.New(fmt.Sprintf("error calling tls.Dial(): %v", err.Error()))
-	}
-	defer conn.Close()
-	client, err := smtp.NewClient(conn, s.config.Host)
-	if err != nil {
-		return errors.New(fmt.Sprintf("error calling smtp.NewClient(): %v", err.Error()))
-	}
-	defer client.Close()
-	err = client.Auth(smtp.PlainAuth("", s.config.Username, s.config.Password, s.config.Host))
-	if err != nil {
-		return errors.New(fmt.Sprintf("error calling SMTP's client.Auth(): %v", err.Error()))
-	}
-	client.Mail(s.from.String())
-	for _, emailAddress := range rcpts {
-		err = client.Rcpt(emailAddress)
-		if err != nil {
-			return errors.New(fmt.Sprintf("error calling rcpt(): %v", err.Error()))
-		}
-	}
-	writer, err := client.Data()
-	if err != nil {
-		return errors.New(fmt.Sprintf("error calling data(): %v", err.Error()))
-	}
-	_, err = writer.Write(message)
-	if err != nil {
-		return errors.New(fmt.Sprintf("error calling writer.Close(): %v", err.Error()))
-	}
-	writer.Close()
-	err = client.Quit()
-	if err != nil {
-		return errors.New(fmt.Sprintf("error quiting client: %v", err.Error()))
 	}
 	return nil
 }
---
+++ smtp-driver_test.go
@@ -0,0 +1,101 @@
+package mailing
+
+import (
+	"crypto/tls"
+	"io/ioutil"
+	"net/mail"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/google/uuid"
+)
+
+func TestSend(t *testing.T) {
+	sDriver := initiateSMTP(&SMTPConfig{
+		Host:     "localhost",
+		Port:     25,
+		Username: "",
+		Password: "",
+		TLSConfig: tls.Config{
+			ServerName:         "localhost",
+			InsecureSkipVerify: true,
+		},
+	})
+	tmpFilePath := filepath.Join(t.TempDir(), uuid.NewString())
+	sDriver.initiateSend = func(from string, rcpts []string, message []byte, conf SMTPConfig) error {
+		file, err := os.Create(tmpFilePath)
+		defer file.Close()
+		if err != nil {
+			t.Error("faild test send")
+		}
+		file.Write(message)
+		return nil
+	}
+
+	sDriver.SetFrom(mail.Address{
+		Name:    "test from name",
+		Address: "from@mail.com",
+	})
+	sDriver.SetTo([]mail.Address{
+		{Name: "test from name1", Address: "from1@mail.com"},
+		{Name: "test from name2", Address: "from2@mail.com"},
+	})
+	sDriver.SetCC([]mail.Address{
+		{Name: "test cc name1", Address: "cc1@mail.com"},
+		{Name: "test cc name2", Address: "cc2@mail.com"},
+	})
+	sDriver.SetBCC([]mail.Address{
+		{Name: "test bcc name1", Address: "bcc1@mail.com"},
+		{Name: "test bcc name2", Address: "bcc2@mail.com"},
+	})
+	sDriver.SetSubject("this is the subject")
+	sDriver.SetPlainTextBody("this is plain text body")
+	sDriver.SetHTMLBody("this is html body")
+	sDriver.SetAttachments([]Attachment{
+		{
+			Name: "attachment name1",
+			Path: "./testingdata/attachment1.md",
+		},
+		{
+			Name: "attachment name2",
+			Path: "./testingdata/attachment2.md",
+		},
+	})
+	sDriver.Send()
+	file, _ := os.Open(tmpFilePath)
+	mBytes, _ := ioutil.ReadAll(file)
+	m := string(mBytes)
+
+	if !strings.Contains(m, `From: "test from name" <from@mail.com>`) {
+		t.Error("Failed test send")
+	}
+	if !strings.Contains(m, `To: "test from name1" <from1@mail.com>;"test from name2" <from2@mail.com>`) {
+		t.Error("Failed test send")
+	}
+	if !strings.Contains(m, `CC: "test cc name1" <cc1@mail.com>;"test cc name2" <cc2@mail.com>`) {
+		t.Error("Failed test send")
+	}
+	if !strings.Contains(m, `Subject: this is the subject`) {
+		t.Error("Failed test send")
+	}
+	if !strings.Contains(m, `Content-Type: text/html; charset="UTF-8"`) {
+		t.Error("Failed test send")
+	}
+	if !strings.Contains(m, `this is html body`) {
+		t.Error("Failed test send")
+	}
+	if !strings.Contains(m, `Content-Disposition: attachment; filename="attachment name1"`) {
+		t.Error("Failed test send")
+	}
+	if !strings.Contains(m, `dGhpcyBpcyBhIHRlc3QgZmlsZSBmb3IgZW1haWwgYXR0YWNobWVudCAx`) {
+		t.Error("Failed test send")
+	}
+	if !strings.Contains(m, `Content-Disposition: attachment; filename="attachment name2"`) {
+		t.Error("Failed test send")
+	}
+	if !strings.Contains(m, `dGhpcyBpcyBhIHRlc3QgZmlsZSBmb3IgZW1haWwgYXR0YWNobWVudCAy`) {
+		t.Error("Failed test send")
+	}
+}
